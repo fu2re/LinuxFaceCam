4e376c5825c9dfa1c1e1c5359c1bc074c1e259ed
fu2re <darkfu2re@gmail.com>
2022-04-27 11:25:42 +0300

    Update facecam.sh

--- facecam.sh
+++ facecam.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-# The following lines re-establish the USB connection to the camera
-# since occassionally the camera needs to be reconnected for ffmpeg
 set -euo pipefail
 IFS=$'\n\t'
-
-# Replace it with your. check it by the following command
-# ls /dev/v4l/by-id/
-FACECAM="/dev/v4l/by-id/usb-Elgato_Elgato_Facecam_FW21K1A16088-video-index0"
 
 # Placeholder video. replace it with yours.
 # For some reason image wont not work
@@ -17,6 +10,7 @@ PLACEHOLDER="/home/fu2re/facecam/vhs.webm"
 VENDOR="0fd9" # Elgato Vendor
 PRODUCT="0078" # Facecam ID
 
+# Variables
 LABEL="Elagto FaceCam"
 DEV=11
 POLL_INTERVAL=1
@@ -24,20 +18,27 @@ TIMEOUT=5
 INUSE=1
 PATTERN="(ffmpeg.*-f v4l2)"
 
-if ! [[ $(lsusb | grep $VENDOR:$PRODUCT) ]]; then
-  exit 0
-fi
+#if ! [[ $(lsusb | grep $VENDOR:$PRODUCT) ]]; then
+#  exit 0
+#fi
+
+# The following lines re-establish the USB connection to the camera
+# since occassionally the camera needs to be reconnected for ffmpeg
 
 function get_cam () {
   FACECAM_ID=$(ls /dev/v4l/by-id/ | grep -i -E "facecam.+index0")
   echo $FACECAM_ID
 }
 
+
+# The following lines re-establish the USB connection to the camera
+# since occassionally the camera needs to be reconnected for ffmpeg
 if ! [[ $(get_cam) ]]; then
   echo "Init camera seq"
   for DIR in $(find /sys/bus/usb/devices/ -maxdepth 1 -type l); do
     if [[ -f $DIR/idVendor && -f $DIR/idProduct &&
           $(cat $DIR/idVendor) == $VENDOR && $(cat $DIR/idProduct) == $PRODUCT ]]; then
+      echo $DIR
       echo 0 | sudo tee -a $DIR/authorized > /dev/null
       sleep 0.5
       echo 1 | sudo tee -a $DIR/authorized > /dev/null
@@ -82,15 +83,15 @@ do
   if [[ $(lsof /dev/video$DEV | grep /dev/video$DEV | grep -v ffmpeg) ]]
   then
     if (( INUSE == 0 )); then
-      echo "stream ON"
+      echo "Stream is currently ON"
       reload_module
-      ffmpeg -f v4l2 -input_format uyvy422 -framerate 60 -video_size 1920x1080 -i $FACECAM -pix_fmt yuyv422 -f v4l2 /dev/video$DEV -loglevel quiet &
+      ffmpeg -f v4l2 -input_format uyvy422 -framerate 60  -i $FACECAM -filter:v "crop=1280:720" -pix_fmt yuyv422 -f v4l2 /dev/video$DEV -loglevel quiet &
       INUSE=1
     fi
   elif (( INUSE == 1 )); then
      sleep $TIMEOUT
      if ! [[ $(lsof /dev/video$DEV | grep /dev/video$DEV | grep -v ffmpeg) ]]; then
-       echo "stream OFF"
+       echo "Stream is currently OFF"
        reload_module
        ffmpeg -stream_loop -1 -re -i $PLACEHOLDER -map 0:v -f v4l2 -input_format uyvy422 -framerate 60 -video_size 1920x1080 -pix_fmt yuyv422 /dev/video$DEV -loglevel quiet &
        INUSE=0
